19c519a1f080fbdabc8f04fa9fe39cbfee6306d2
professinalsidd <professionalsiddmail@gmail.com>
2024-11-21 18:37:41 +0530

    feat: nav bar make it

---
+++ src/app/home/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div>Hello Home</div>;
+};
+
+export default HomePage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import HomePage from "./home/home";
+import NavbarComp from "@/components/navbar/navbar";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <NavbarComp />
+      <HomePage />
+    </div>
+  );
 }
